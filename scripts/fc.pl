e5880c9534b221633481816a5fd50b1dadd73a4a
Slaven Rezic <slaven.rezic@idealo.de>
2022-08-25 15:17:13 +0200

    longer test files for FC

--- scripts/fc.pl
+++ scripts/fc.pl
@@ -8,12 +8,12 @@ my $doit = Doit->init;
 
 my $tmp1 = File::Temp->new;
 $tmp1->print(#"x"x1024 . " " .
-    ("same line\n" x 20) . "old content\n" . ("same line\n" x 20));
+    ("same line\n" x 20) . ("old content\n" . ("same line\n" x 20)) x 2);
 $tmp1->close;
 
 my $tmp2 = File::Temp->new;
 $tmp2->print(#"x"x1024 . " " .
-    ("same line\n" x 20) . "new content\n" . ("same line\n" x 20));
+    ("same line\n" x 20) . ("new content\n" . ("same line\n" x 20)) x 2);
 $tmp2->close;
 
 eval { $doit->system("fc", "$tmp1", "$tmp2") };
