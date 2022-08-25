1bd6457d4cb112114721a631a1bb6ea971b654e1
Slaven Rezic <slaven.rezic@idealo.de>
2022-08-25 11:04:59 +0200

    try without IO::Select

--- scripts/open3.pl
+++ scripts/open3.pl
@@ -51,31 +51,44 @@ info "open3 started with pid: $pid";
 info "stdin provided";
 	}
 
-	my $sel = IO::Select->new;
-	$sel->add($chld_out);
-	$sel->add($chld_err);
+	my %buf = ($chld_out => '', $chld_err => '');
+
+	if (0) {
+	    my $sel = IO::Select->new;
+	    $sel->add($chld_out);
+	    $sel->add($chld_err);
 info "IO::Select on chld_out and _err";
 
-	my %buf = ($chld_out => '', $chld_err => '');
-	while(my @ready_fhs = $sel->can_read()) {
+	    while(my @ready_fhs = $sel->can_read()) {
 info "can_read: @ready_fhs";
-	    for my $ready_fh (@ready_fhs) {
-		my $buf = '';
+                for my $ready_fh (@ready_fhs) {
+		    my $buf = '';
 info "about to read $ready_fh";
-		while (sysread $ready_fh, $buf, 1024, length $buf) {
+		    while (sysread $ready_fh, $buf, 1024, length $buf) {
 info "in while-sysread loop, length of buf=" . length($buf);
-		}
-		if ($buf eq '') { # eof
+	 	    }
+		    if ($buf eq '') { # eof
 info "found eof";
-		    $sel->remove($ready_fh);
-		    $ready_fh->close;
-		    last if $sel->count == 0;
-		} else {
+		        $sel->remove($ready_fh);
+		        $ready_fh->close;
+		        last if $sel->count == 0;
+		    } else {
 info "append " . length($buf) . " bytes to buf";
-		    $buf{$ready_fh} .= $buf;
+		        $buf{$ready_fh} .= $buf;
+                    }
 		}
 	    }
+	} else {
+	    while(<$chld_out>) {
+info "append " . length($_) . " bytes to buf (stdout)";
+                $buf{$chld_out} .= $_;
+            }
+	    while(<$chld_err>) {
+info "append " . length($_) . " bytes to buf (stderr)";
+                $buf{$chld_err} .= $_;
+	    }
 	}
+
 info "waiting for process";
 	waitpid $pid, 0;
 info "all done";
