a48ed3631b6b33a5e5dee0f520daadf535d9048d
Slaven Rezic <slaven.rezic@idealo.de>
2022-08-25 17:43:00 +0200

    try without exec

--- scripts/windows-signals.pl
+++ scripts/windows-signals.pl
@@ -3,23 +3,26 @@
 use strict;
 use warnings;
 use autodie;
-use File::Temp;
 
-my $script = File::Temp->new;
-$script->print(<<"EOF" . <<'EOF');
-#!$^X
-EOF
-warn "sub process started...\n";
-while() { sleep 1 }
-EOF
-$script->close;
+#use File::Temp;
+#my $script = File::Temp->new;
+#$script->print(<<"EOF" . <<'EOF');
+##!$^X
+#EOF
+#warn "sub process started...\n";
+#while() { sleep 1 }
+#EOF
+#$script->close;
 
 my $child_pid = fork;
 if ($child_pid == 0) {
     #my @cmd = ($^X, '-e', 'warn "sub process started...\n"; while(){ sleep 1 }');
-    my @cmd = ($^X, "$script");
-    warn "Run @cmd...\n";
-    exec(@cmd);
+    #my @cmd = ($^X, "$script");
+    #warn "Run @cmd...\n";
+    #exec(@cmd);
+    warn "sub process started...\n";
+    while() { sleep 1 }
+    exit; # never reached
 }
 
 sleep 2;
